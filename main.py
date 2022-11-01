3c3f13f02fa700ad69f285561e898befecd299df
Ali Öztürk <84031734+traaz@users.noreply.github.com>
2022-11-01 21:04:58 +0300

    test in postman

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 from flask import Flask,jsonify
 
+#create blockchain
 class Blockchain:
 
     def __init__(self):
@@ -52,3 +53,32 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+
+#creating a web app
+app = Flask(__name__)
+
+blockchain=Blockchain()
+#mining a new block
+@app.route("/mine_block", methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congract you mined a block',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash' : block['previous_hash']}
+    return jsonify(response)
+#get block
+@app.route("/get_chain", methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length' : len(blockchain.chain)}
+    return jsonify(response)
+
+
+app.run(host = '0.0.0.0', port=5000)
